63a03e54c90c347d38c36fabfe6081a6bad60d44
wajdi19 <wajdibejaoui50@gmail.com>
2023-12-20 16:01:26 +0100

    update reset-password

--- ProjetSpring-main/ProjetSpring-main/springBoot-main/project/src/main/java/com/example/project/controller/ForgotPasswordController.java
+++ ProjetSpring-main/ProjetSpring-main/springBoot-main/project/src/main/java/com/example/project/controller/ForgotPasswordController.java
@@ -7,14 +7,18 @@ import com.example.project.repositories.UserRepository;
 import com.example.project.service.PasswordResetTokenService;
 import com.example.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForgotPasswordController {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
 
@@ -31,11 +35,12 @@ public class ForgotPasswordController {
     @PostMapping("/forgot-password")
     public String processForgotPassword(@RequestParam String email, Model model) {
         User user = userRepository.findByEmail(email);
-
         if (user != null) {
             PasswordResetToken token = tokenService.createToken(user);
+
             // Vous pouvez envoyer le lien de réinitialisation par e-mail ici
             model.addAttribute("message", "Un lien de réinitialisation a été envoyé à votre adresse e-mail.");
+            model.addAttribute("token", token.getToken());
         } else {
             model.addAttribute("error", "Aucun utilisateur n'a été trouvé avec cet e-mail.");
         }
@@ -43,26 +48,25 @@ public class ForgotPasswordController {
         return "reset-password";
     }
 
-    @GetMapping("/reset-password")
-    public String showResetPasswordForm(@RequestParam String token, Model model) {
-        PasswordResetToken resetToken = passwordResetTokenRepository.findByTokenQuery(token);
+//    @GetMapping("/reset-password")
+//    public String showResetPasswordForm(@RequestParam String token, Model model) {
+//        PasswordResetToken resetToken = passwordResetTokenRepository.findByTokenQuery(token);
+//
+//      f  if (resetToken != null && !resetToken.isExpired()) {
+//            model.addAttribute("token", token);
+//            return "reset-password";
+//        } else {
+//            model.addAttribute("error", "Le lien de réinitialisation est invalide ou a expiré.");
+//            return "forgot-password";
+//        }
+//    }
 
-        if (resetToken != null && !resetToken.isExpired()) {
-            model.addAttribute("token", token);
-            return "reset-password";
-        } else {
-            model.addAttribute("error", "Le lien de réinitialisation est invalide ou a expiré.");
-            return "forgot-password";
-        }
-    }
-
-    @PostMapping("/reset-passwordd")
+    @PostMapping("/reset-password")
     public String processResetPassword(@RequestParam String token, @RequestParam String password, Model model) {
         PasswordResetToken resetToken = passwordResetTokenRepository.findByTokenQuery(token);
-
         if (resetToken != null && !resetToken.isExpired()) {
             User user = resetToken.getUser();
-            user.setPassword(password);
+            user.setPassword(passwordEncoder.encode(password));
             userRepository.save(user);
             tokenService.deleteToken(resetToken);
             model.addAttribute("message", "Votre mot de passe a été réinitialisé avec succès.");
@@ -70,6 +74,6 @@ public class ForgotPasswordController {
             model.addAttribute("error", "Le lien de réinitialisation est invalide ou a expiré.");
         }
 
-        return "login";
+        return "SignIn";
     }
 }
